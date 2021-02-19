a7f1986ab463a0fb26ad8cbdc64016112d34ada6
azadebrahimian <azadebrahimian@gmail.com>
2021-02-19 17:44:52 +0000

    updated deploy, nginx, service, start, package-lock

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-export SECRET_KEY_BASE=W68eso5YQOlbtvSNUR50N/HDWj6IaEhAwMR3LtzuBEQAefwYVbX84bvoTA7XtiGi
 export MIX_ENV=prod
-export PORT=4790
+export PORT=4791
 export NODEBIN=`pwd`/assets/node_modules/.bin
 export PATH="$PATH:$NODEBIN"
 
